ff56bdc93cdc70dba9979c24e11955ab6bf8f9c8
hamburghammer <hamburghammer@gmail.com>
2018-09-25 18:01:27 +0200

    chai-dom deinstalliert -- kein import

--- testTodoApp.js
+++ testTodoApp.js
@@ -1,6 +1,6 @@
 var expect = require('chai').expect;
 var chai = require('chai')
-chai.use(require('chai-dom'))
+expect.use(require('chai-dom').use)
 
 var addTodoToList = require("./todoApp.js").addTodoToList;
 var getTodoList = require("./todoApp.js").getTodoList;
@@ -8,7 +8,6 @@ var foo = require('./todoApp.js').foo;
 
 describe('addTodoToList', function() {
   it('should return a arry', function() {
-    var exampleList = [];
     expect(getTodoList().length).to.equal(0);
     addTodoToList("foo");
     expect(getTodoList().length).to.equal(1);
