8eec99f39e603d914ff5918d72e5c8e11cddc3c9
hamburghammer <hamburghammer@gmail.com>
2018-09-27 12:59:21 +0200

    getLocalStorage setLocalStorage test erweitert

--- test/testTodoApp.js
+++ test/testTodoApp.js
@@ -2,12 +2,16 @@ var expect = require('chai').expect;
 var assert = require('chai').assert;
 require('dom-test');
 
+var LocalStorage = require('node-localstorage').LocalStorage;
+localStorage = new LocalStorage('./scratch');
+
 var todoApp = "../todoApp.js";
 
 var addTodoToList = require(todoApp).addTodoToList;
 var getTodoList = require(todoApp).getTodoList;
 var foo = require(todoApp).foo;
-//var localStorage = require('./todoApp.js').localStorage;
+var setLocalStorage = require('../todoApp.js').setLocalStorage;
+var getLocalStorage = require('../todoApp.js').getLocalStorage;
 
 describe('todoList', function() {
   it('should return a arry', function() {
@@ -23,3 +27,10 @@ describe('foo', function() {
     expect(foo("foo")).to.equal("foo");
   });
 });
+
+describe('localStorage data should be returned as a string', () =>{
+  it('saves in localStorage', () =>{
+    setLocalStorage(["foo", 12, "foo bar"]);
+    expect(getLocalStorage()).to.deep.equal(["foo", 12, "foo bar"])
+  })
+})
